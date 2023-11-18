c06fb3fcceef0238b76d277c4f67102b0da75948
JadKHaddad <jadkhaddad@gmail.com>
2023-11-18 20:45:29 +0100

    api docs Skel.

---
+++ push/push_server/README.md
@@ -0,0 +1,1 @@
+# AlertmanagerExt Push Server
--- push/push_server/src/main.rs
+++ push/push_server/src/main.rs
@@ -1,15 +1,20 @@
 use aide::{
     axum::{
-        routing::{get, post},
+        routing::{get, post_with},
         ApiRouter, IntoApiResponse,
     },
     openapi::{Info, OpenApi},
+    redoc::Redoc,
+    transform::TransformOperation,
+    OperationIo,
 };
 use anyhow::{Context, Result as AnyResult};
-use axum::{extract::State, Extension, Json};
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Extension, Json};
 use models::AlermanagerPush;
 use plugins_definitions::Plugin;
 use push_definitions::Push;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
 use std::{net::SocketAddr, ops::Deref, sync::Arc};
 
 trait Serverble: Push + Plugin {}
@@ -31,14 +36,60 @@ impl Deref for ApiV1State {
     }
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize, JsonSchema, OperationIo)]
+#[serde(rename_all = "camelCase")]
+pub enum PushStatus {
+    Full,
+    Partial,
+    Failed,
+}
+
+impl PushStatus {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            PushStatus::Full => StatusCode::ACCEPTED,
+            PushStatus::Partial => StatusCode::MULTI_STATUS,
+            PushStatus::Failed => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, JsonSchema, OperationIo)]
+#[serde(rename_all = "camelCase")]
+pub struct PushResponse {
+    pub status: PushStatus,
+}
+
+impl PushResponse {
+    fn create_docs(op: TransformOperation) -> TransformOperation {
+        op.description("Push alerts to plugins")
+            .response_with::<201, Json<Self>, _>(|res| {
+                res.description("All alerts were pushed successfully")
+            })
+            .response_with::<207, Json<Self>, _>(|res| {
+                res.description("Some alerts were pushed successfully")
+            })
+            .response_with::<500, Json<Self>, _>(|res| res.description("Failed to push alerts"))
+    }
+}
+
+impl IntoResponse for PushResponse {
+    fn into_response(self) -> axum::response::Response {
+        (self.status.status_code(), Json(self)).into_response()
+    }
+}
+
 async fn push(
     State(state): State<ApiV1State>,
     Json(alertmanager_push): Json<AlermanagerPush>,
-) -> impl IntoApiResponse {
+) -> PushResponse {
     for plugin in &state.plugins {
         plugin.push(&alertmanager_push).await.unwrap();
     }
-    "PUSH"
+
+    PushResponse {
+        status: PushStatus::Full,
+    }
 }
 
 async fn serve_api(Extension(api): Extension<OpenApi>) -> impl IntoApiResponse {
@@ -51,20 +102,20 @@ async fn main() -> AnyResult<()> {
     for plugin in &plugins {
         plugin.initialize().await.unwrap();
     }
-    let state = ApiV1State {
+    let api_v1_state = ApiV1State {
         inner: Arc::new(ApiV1StateInner { plugins }),
     };
 
     let v1 = ApiRouter::new()
-        .api_route("/push", post(push))
-        .with_state(state.clone());
+        .api_route("/push", post_with(push, PushResponse::create_docs))
+        .with_state(api_v1_state);
 
     let api_v1 = ApiRouter::new().nest("/v1", v1);
 
     let app = ApiRouter::new()
+        .route("/redoc", Redoc::new("/api.json").axum_route())
         .route("/api.json", get(serve_api))
-        .nest_api_service("/api", api_v1)
-        .with_state(state);
+        .nest_api_service("/api", api_v1);
 
     let mut open_api = OpenApi {
         info: Info {
