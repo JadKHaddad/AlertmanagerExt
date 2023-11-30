9786e44da47abde1d703706a80c56aa704c8830c
JadKHaddad <jadkhaddad@gmail.com>
2023-11-30 11:56:06 +0100

    prometheus metrics

--- push/push_server/src/routes/metrics.rs
+++ push/push_server/src/routes/metrics.rs
@@ -3,7 +3,7 @@ use axum::extract::State;
 
 /// Prometheus metrics endpoint
 #[utoipa::path(get, path = "/metrics", tag = "metrics", responses(
-    (status = 200, description = "Metrics."),
+    (status = 200, description = "Prometheus metrics."),
     (status = 500, description = "Iternal server error.", body = [ErrorResponse])
 ))]
 pub async fn metrics(State(state): State<ApiState>) -> Result<String, ErrorResponse> {
