3648961e30db81cc7a30536842bad95306b6d7d3
JadKHaddad <jadkhaddad@gmail.com>
2023-12-02 21:16:21 +0100

    fixed the playground

--- plugins/postgres_plugin/sql_playground.sql
+++ plugins/postgres_plugin/sql_playground.sql
@@ -1,32 +1,27 @@
-select *
-from (
-        select
-            grouped_with_labels.id,
-            grouped_with_labels.labels,
-            array_agg( (
-                    alert_annotation.name,
-                    alert_annotation.value
+SELECT *
+FROM (
+        SELECT
+            labels_per_alert.id,
+            labels_per_alert.labels,
+            ARRAY_AGG( (
+                    annotations.name,
+                    annotations.value
                 )
-            ) as annotations
-        from (
-                select
-                    alert.id,
-                    array_agg( (
-                            alert_label.name,
-                            alert_label.value
-                        )
-                    ) as labels
-                from alert
-                    inner join assign_alert_label ON assign_alert_label.alert_id = alert.id
-                    inner join alert_label ON alert_label.id = assign_alert_label.alert_label_id
-                group by
-                    alert.id,
-                    alert.fingerprint
-            ) as grouped_with_labels
-            inner join assign_alert_annotation ON assign_alert_annotation.alert_id = grouped_with_labels.id
-            inner join alert_annotation ON alert_annotation.id = assign_alert_annotation.alert_annotation_id
-        group by
-            grouped_with_labels.id,
-            grouped_with_labels.labels
-    ) as grouped_with_lalbels_and_annotations
-    inner join alert on alert.id = grouped_with_lalbels_and_annotations.id;
+            ) AS annotations
+        FROM (
+                SELECT
+                    alerts.id,
+                    ARRAY_AGG( (labels.name, labels.value)) AS labels
+                FROM alerts
+                    INNER JOIN alerts_labels ON alerts_labels.alert_id = alerts.id
+                    INNER JOIN labels ON labels.id = alerts_labels.label_id
+                GROUP BY
+                    alerts.id
+            ) AS labels_per_alert
+            INNER JOIN alerts_annotations ON alerts_annotations.alert_id = labels_per_alert.id
+            INNER JOIN annotations ON annotations.id = alerts_annotations.annotation_id
+        GROUP BY
+            labels_per_alert.id,
+            labels_per_alert.labels
+    ) AS labels_and_annotations_per_alert
+    INNER JOIN alerts ON alerts.id = labels_and_annotations_per_alert.id;
