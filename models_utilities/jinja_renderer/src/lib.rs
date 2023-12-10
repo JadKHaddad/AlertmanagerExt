2790aea2477bef08df760768a5c47bb76d56099d
JadKHaddad <jadkhaddad@gmail.com>
2023-12-10 18:54:09 +0100

    jinja renderer with Environment<'static>

--- models_utilities/jinja_renderer/src/lib.rs
+++ models_utilities/jinja_renderer/src/lib.rs
@@ -4,36 +4,42 @@ use std::path::Path;
 use thiserror::Error as ThisError;
 
 #[derive(ThisError, Debug)]
-pub enum RendererFromFileError {
+pub enum NewRendererError {
     #[error("Failed to read file: {0}")]
     IoError(
         #[source]
         #[from]
         tokio::io::Error,
     ),
+    #[error("Failed to configure template: {0}")]
+    MinijinjaError(
+        #[source]
+        #[from]
+        minijinja::Error,
+    ),
 }
 
 pub struct Renderer {
-    template: String,
+    env: Environment<'static>,
 }
 
 impl Renderer {
-    pub async fn new_from_file(file: impl AsRef<Path>) -> Result<Self, RendererFromFileError> {
-        let template = tokio::fs::read_to_string(file).await?;
-        Ok(Self { template })
+    pub async fn new_from_str(template: impl Into<String>) -> Result<Self, NewRendererError> {
+        let template = template.into();
+        let mut env = Environment::new();
+        minijinja_contrib::add_to_environment(&mut env);
+        env.add_template_owned("push", template)?;
+
+        Ok(Self { env })
     }
 
-    pub async fn new_from_str(template: impl Into<String>) -> Result<Self, RendererFromFileError> {
-        Ok(Self {
-            template: template.into(),
-        })
+    pub async fn new_from_file(file: impl AsRef<Path>) -> Result<Self, NewRendererError> {
+        let template = tokio::fs::read_to_string(file).await?;
+        Self::new_from_str(template).await
     }
 
     pub fn render(&self, push: &AlertmanagerPush) -> Result<String, minijinja::Error> {
-        let mut env = Environment::new();
-        minijinja_contrib::add_to_environment(&mut env);
-        env.add_template("push", &self.template)?;
-        let template = env.get_template("push")?;
+        let template = self.env.get_template("push")?;
         let context = context! {
             push => push
         };
@@ -50,7 +56,7 @@ mod tests {
     #[tokio::test]
     async fn render_from_str() {
         let push = generate_random_alertmanager_pushes(1).pop().unwrap();
-        let renderer = Renderer::new_from_str(include_str!("../template.j2"))
+        let renderer = Renderer::new_from_str(include_str!("../example-template.j2"))
             .await
             .expect("failed to create renderer");
 
@@ -62,7 +68,7 @@ mod tests {
     #[tokio::test]
     async fn render_from_file() {
         let push = generate_random_alertmanager_pushes(1).pop().unwrap();
-        let renderer = Renderer::new_from_file(Path::new("template.j2"))
+        let renderer = Renderer::new_from_file(Path::new("example-template.j2"))
             .await
             .expect("failed to create renderer");
 
